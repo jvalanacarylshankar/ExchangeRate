65ed72dbb1a9587bd76a12c2f8ecfa4f4ba2e4c2
jvalanacarylshankar <jvalana.shankar152@gmail.com>
2019-05-26 14:51:14 -0700

    added more test

--- index.js
+++ index.js
@@ -5,55 +5,64 @@ const requestAsync = Promise.promisify(require('request'));
 const AgentKeepAlive = require('agentkeepalive');
 
 const httpAgent = new AgentKeepAlive({
-    timeout: 60000,
-    freeSocketTimeout: 30000 // free socket keepalive for 30 seconds
+  timeout: 60000,
+  freeSocketTimeout: 30000 // free socket keepalive for 30 seconds
 });
 
-const url = "http://api.exchangeratesapi.io/latest";
-
-const _getBaseRate = (base, symbols) => {
-    if (!base) {
-        return Promise.reject(new Error("need a base currency"));
-    }
-
-    if (!symbols) {
-        return Promise.reject(new Error("need at least one currency to convert against"));
-    }
-
-    const requestOptions = {
-        agent: httpAgent,
-        time: true,
-        method: 'GET',
-        uri: `${url}?base=${base}&symbols=${symbols}`,
-        json: true
-    };
-
-    return requestAsync(requestOptions)
-        .then((response) => {
-            return response;
-        })
-        .catch((err) => {
-            console.log("Request failed: ", JSON.stringify({ error: err, stack: err.stack }));
-            throw err;
-        });
+const config = {
+  appId: "786d5594b71e4ab0ae71c5a9ae2c7f8a",
+  get: `http://openexchangerates.org/latest.json`,
 };
 
-const _convert = (value, base, symbol) => {
-    if (!value) {
-        return Promise.reject(new Error("need a value"));
-    }
+const _getBaseRate = (base, symbols) => {
+  if (!base) {
+    return Promise.reject(new Error("need a base currency"));
+  }
 
-    return _getBaseRate(base, symbol).then((result) => {
-        if (result) {
-            return result.rates.symbol * value;
-        }
+  if (typeof base !== "string" || base.includes(",") || base.includes(" ")) {
+    return Promise.reject(new Error("need a single string base currency"));
+  }
+
+  if (!symbols) {
+    return Promise.reject(new Error("need at least one currency to convert against"));
+  }
+
+  const requestOptions = {
+    agent: httpAgent,
+    time: true,
+    method: 'GET',
+    uri: `${config.get}?app_id=${config.appId}&base=${base}&symbols=${symbols.toString()}`,
+    json: true
+  };
+
+  return requestAsync(requestOptions)
+    .then((response) => {
+      return Promise.resolve(response.rates);
     })
-        .catch((err) => {
-            return Promise.reject(err);
-        });
+    .catch((err) => {
+      console.log("Request failed: ", JSON.stringify({ error: err, stack: err.stack }));
+      throw err;
+    });
+};
+
+const _convert = (value, from, to) => {
+  if (!value) {
+    return Promise.reject(new Error("need a value"));
+  }
+
+  if (typeof to !== "string" || to.includes(",") || to.includes(" ")) {
+    return Promise.reject(new Error("need a single string currency to convert to"));
+  }
+
+  return _getBaseRate(from, to).then((result) => {
+    return Promise.resolve({ rate: result[to] * value })
+  })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
 };
 
 module.exports = {
-    getBaseRate: _getBaseRate,
-    convert: _convert
+  getBaseRate: _getBaseRate,
+  convert: _convert
 };
--- test/test.js
+++ test/test.js
@@ -18,42 +18,76 @@ describe('getBaseRate', () => {
     return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a base currency');
   });
 
+  it('should fail when base is not string', function () {
+    const result = exchangeRate.getBaseRate(["USD"], null);
+
+    return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a single string base currency');
+  });
+
   it('should fail when symbols is null', function () {
     const result = exchangeRate.getBaseRate("USD", null);
 
     return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need at least one currency to convert against');
   });
 
-  /*it('should return rate', function () {
-    const result = exchangeRate.getBaseRate("CAD", "INR");
+  it('should return rate for a single conversion', function () {
+    const result = exchangeRate.getBaseRate("USD", "CAD");
 
     return expect(result).to.eventually.be.not.null;
-  });*/
+  });
+
+  it('should return rate for lower case currencies', function () {
+    const result = exchangeRate.getBaseRate("usd", "cad");
+
+    return expect(result).to.eventually.be.not.null;
+  });
+
+  it('should return rate for a multiple string conversion', function () {
+    const result = exchangeRate.getBaseRate("CAD", "USD,INR");
+
+    return expect(result).to.eventually.be.not.null;
+  });
+
+  it('should return rate for a multiple object conversion', function () {
+    const result = exchangeRate.getBaseRate("CAD", ["USD", "INR"]);
+
+    return expect(result).to.eventually.be.not.null;
+  });
 });
 
 describe('convert', () => {
 
-    it('should fail when value is null', function () {
-      const result = exchangeRate.convert(null, "USD", "CAD");
-  
-      return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a value');
-    });
+  it('should fail when value is null', function () {
+    const result = exchangeRate.convert(null, "USD", "CAD");
 
-    it('should fail when base is null', function () {
-        const result = exchangeRate.convert(10, null, "CAD");
-    
-        return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a base currency');
-      });
-  
-    it('should fail when symbols is null', function () {
-        const result = exchangeRate.convert(10, "USD", null);
-  
-      return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need at least one currency to convert against');
-    });
-  
-    /*it('should return rate', function () {
-      const result = exchangeRate.getBaseRate("CAD", "INR");
-  
-      return expect(result).to.eventually.be.not.null;
-    });*/
+    return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a value');
   });
+
+  it('should fail when base is null', function () {
+    const result = exchangeRate.convert(10, null, "CAD");
+
+    return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a base currency');
+  });
+
+  it('should fail when symbols is null', function () {
+    const result = exchangeRate.convert(10, "USD", null);
+
+    return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a single string currency to convert to');
+  });
+
+  it('should fail when symbols is not string', function () {
+    const result = exchangeRate.convert(10, "USD", ["CAD"]);
+
+    return expect(result).to.eventually.be.rejected.and.to.have.property('message', 'need a single string currency to convert to');
+  });
+
+  it('should return rate', function () {
+    const result = exchangeRate.convert(10, "USD", "CAD");
+
+    result.then(item => {
+      console.log(item)
+      return true;
+    })
+    //return expect(result).to.eventually.be.not.null;
+  });
+});
